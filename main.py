3309882ee73b248d64ec5c34a8688e87b98aa925
Nikita <75705735+gaksc@users.noreply.github.com>
2023-01-18 11:37:53 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import os
+import random
+
 import pygame
 import sys
 from random import randint, choice
-
+import time
+n = 0
+lunki = [(30, 130), (180, 130), (330, 130), (30, 280), (180, 280), (330, 280), (30, 430), (180, 430), (330, 430)]
 def load_image(name, colorkey=None):
-    fullname = os.path.join('../pythonProject10/data', name)
+    fullname = os.path.join('../UBEYBOBRA-main/data', name)
     image = pygame.image.load(fullname).convert()
     image.set_colorkey(colorkey)
     return image
-
 pygame.init()
-
-k = pygame.mixer.Sound('../pythonProject10/data/norm_music.mp3')
+zawarudo = False
+k = pygame.mixer.Sound('../UBEYBOBRA-main/data/norm_music.mp3')
 k.play(-1)
-
-sound = pygame.mixer.Sound('../pythonProject10/data/shot.mp3')
+sound = pygame.mixer.Sound('../UBEYBOBRA-main/data/shot.mp3')
+zaw = pygame.mixer.Sound('../UBEYBOBRA-main/data/zaw.mp3')
 
 
 pygame.time.set_timer(pygame.USEREVENT, 700)
@@ -22,14 +25,14 @@ font = pygame.font.SysFont('Consolas', 20)
 win = pygame.display.set_mode((450, 550))
 pygame.display.set_caption('Убей бобра (Играть со звуком!)')
 hole_list = [False] * 9
-
+dio = load_image("dio2.png")
 clock = pygame.time.Clock()
 
 score = 0
 
 bobr = load_image("bobr_alive.jpg")
 bobrs_list = [load_image("bobr_alive.jpg"), load_image("bobr_hunter.png"), load_image("bobr_murder.png"),
-              load_image("bobr_boom.png"), load_image("bobr_jeday.png"), load_image("bobr_soldier.png"),
+              load_image("bobr_battotai.png"), load_image("bobr_jeday.png"), load_image("bobr_soldier.png"),
               load_image("bobr_grenadier.png"), load_image("bobr_spec.png")]
 hole_img = load_image("hole.png")
 bg_img = load_image("BG.png")
@@ -49,7 +52,7 @@ if colorkey is not None:
 else:
     krest = krest.convert_alpha()
 pygame.mouse.set_visible(False)
-MANUAL_CURSOR = pygame.image.load('data/3.png').convert_alpha()
+MANUAL_CURSOR = pygame.image.load('data/coursor.png').convert_alpha()
 
 while True:
     for event in pygame.event.get():
@@ -57,7 +60,7 @@ while True:
             if hole_list[bobr_hole]:
                 score -= 1
                 score = max(score, 0)
-                pygame.mixer.music.load('../pythonProject10/data/BOSS_MUSIC.mp3')
+                pygame.mixer.music.load('../UBEYBOBRA-main/data/BOSS_MUSIC.mp3')
             hole_list[bobr_hole] = False
             new_bobr_hole = randint(0, 8)
             if new_bobr_hole == bobr_hole:
@@ -73,6 +76,7 @@ while True:
 
 
         if event.type == pygame.MOUSEBUTTONUP:
+            sound.play()
             mouse_pos = pygame.mouse.get_pos()
             if mouse_pos[0] in list(range(10, 60)) and mouse_pos[1] in list(range(10, 60)):
                 pygame.quit()
@@ -80,93 +84,99 @@ while True:
             if mouse_pos[0] in list(range(30, 120)) and mouse_pos[1] in list(range(130, 220)) and hole_list[0] and not isBoss:
                 hole_list[0] = False
                 score += 1
-                sound.play()
+
             elif mouse_pos[0] in list(range(180, 270)) and mouse_pos[1] in list(range(130, 220)) and hole_list[1] and not isBoss:
                 hole_list[1] = False
                 score += 1
-                sound.play()
+
             elif mouse_pos[0] in list(range(330, 420)) and mouse_pos[1] in list(range(130, 220)) and hole_list[2] and not isBoss:
                 hole_list[2] = False
                 score += 1
-                sound.play()
+
             elif mouse_pos[0] in list(range(30, 120)) and mouse_pos[1] in list(range(280, 370)) and hole_list[3] and not isBoss:
                 hole_list[3] = False
                 score += 1
-                sound.play()
+
             elif mouse_pos[0] in list(range(180, 270)) and mouse_pos[1] in list(range(280, 370)) and hole_list[4] and not isBoss:
                 hole_list[4] = False
                 score += 1
-                sound.play()
+
             elif mouse_pos[0] in list(range(330, 420)) and mouse_pos[1] in list(range(280, 370)) and hole_list[5] and not isBoss:
                 hole_list[5] = False
                 score += 1
-                sound.play()
+
             elif mouse_pos[0] in list(range(30, 120)) and mouse_pos[1] in list(range(430, 520)) and hole_list[6] and not isBoss:
                 hole_list[6] = False
                 score += 1
-                sound.play()
+
             elif mouse_pos[0] in list(range(180, 270)) and mouse_pos[1] in list(range(430, 520)) and hole_list[7] and not isBoss:
                 hole_list[7] = False
                 score += 1
-                sound.play()
+
             elif mouse_pos[0] in list(range(330, 420)) and mouse_pos[1] in list(range(430, 520)) and hole_list[8] and not isBoss:
                 hole_list[8] = False
                 score += 1
-                sound.play()
+
             elif not isBoss:
                 score -= 1
-            elif mouse_pos[0] in list(range(30, 420)) and mouse_pos[1] in list(range(120, 520)):
+            elif mouse_pos[0] in list(range(m[0], m[0] + 98)) and mouse_pos[1] in list(range(m[1], m[1] + 101)):
                 score += 1
                 if score == 20:
                     isBoss = False
-                    pygame.mixer.music.load('../pythonProject10/data/norm_music.mp3')
+                    pygame.mixer.music.load('../UBEYBOBRA-main/data/norm_music.mp3')
                     pygame.mixer.music.play(-1)
                 elif score == 40:
                     isBoss = False
-                    pygame.mixer.music.load('../pythonProject10/data/norm_music.mp3')
+                    pygame.mixer.music.load('../UBEYBOBRA-main/data/norm_music.mp3')
                     pygame.mixer.music.play(-1)
                 elif score == 60:
                     isBoss = False
-                    pygame.mixer.music.load('../pythonProject10/data/norm_music.mp3')
+                    pygame.mixer.music.load('../UBEYBOBRA-main/data/norm_music.mp3')
                     pygame.mixer.music.play(-1)
                 elif score == 80:
                     isBoss = False
-                    pygame.mixer.music.load('../pythonProject10/data/norm_music.mp3')
+                    pygame.mixer.music.load('../UBEYBOBRA-main/data/norm_music.mp3')
                     pygame.mixer.music.play(-1)
                 elif score == 100:
                     isBoss = False
-                    pygame.mixer.music.load('../pythonProject10/data/norm_music.mp3')
+                    pygame.mixer.music.load('../UBEYBOBRA-main/data/norm_music.mp3')
                     pygame.mixer.music.play(-1)
             score = max(score, 0)
             if score == 10 and not kill_boss:
                 k.stop()
                 pygame.time.set_timer(pygame.USEREVENT, 0)
                 isBoss = True
-                pygame.mixer.music.load('../pythonProject10/data/BOSS_MUSIC.mp3')
-                pygame.mixer.music.play(-1)
+                m = random.choice(lunki)
+                pygame.mixer.music.load('../UBEYBOBRA-main/data/BOSS_MUSIC.mp3')
+                if zawarudo:
+                    pygame.mixer.music.play(-1)
 
             if score == 30:
+                k.stop()
                 pygame.time.set_timer(pygame.USEREVENT, 0)
                 isBoss = True
-                pygame.mixer.music.load('../pythonProject109/data/BOSS_MUSIC.mp3')
+                pygame.mixer.music.load('../UBEYBOBRA-main/data/BOSS_MUSIC.mp3')
                 pygame.mixer.music.play(-1)
 
             if score == 50:
+                k.stop()
                 pygame.time.set_timer(pygame.USEREVENT, 0)
                 isBoss = True
-                pygame.mixer.music.load('../pythonProject10/data/BOSS_MUSIC.mp3')
+                pygame.mixer.music.load('../UBEYBOBRA-main/data/BOSS_MUSIC.mp3')
                 pygame.mixer.music.play(-1)
 
             if score == 70:
+                k.stop()
                 pygame.time.set_timer(pygame.USEREVENT, 0)
                 isBoss = True
-                pygame.mixer.music.load('../pythonProject10/data/BOSS_MUSIC.mp3')
+                pygame.mixer.music.load('../UBEYBOBRA-main/data/BOSS_MUSIC.mp3')
                 pygame.mixer.music.play(-1)
 
             if score == 90:
+                k.stop()
                 pygame.time.set_timer(pygame.USEREVENT, 0)
                 isBoss = True
-                pygame.mixer.music.load('../pythonProject10/data/BOSS_MUSIC.mp3')
+                pygame.mixer.music.load('../UBEYBOBRA-main/data/BOSS_MUSIC.mp3')
                 pygame.mixer.music.play(-1)
             pygame.time.set_timer(pygame.USEREVENT, max(1000 - score * 2, 500))
 
@@ -223,7 +233,18 @@ while True:
         win.blit(bobr, (330, 430))
 
     if isBoss:
-        win.blit(boss_img, (30, 120))
+        #if score == 19:
+            #if not zawarudo:
+                #pygame.mixer.stop()
+                #k.stop()
+                #m = (0, 0)
+                #win.blit(dio, m)
+                #if n == 0:
+                #    zaw.play(-1)
+                #score %= 12
+                #n += 1
+        #else:
+        win.blit(boss_img, m)
 
     if pygame.mouse.get_focused():
         win.blit(MANUAL_CURSOR, (pygame.mouse.get_pos()))
